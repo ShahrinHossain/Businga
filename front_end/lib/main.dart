8550d45acf5d18c3a2ee5698826bdddb906abe64
AbDhrubo <abubakorsiddiquedhrubo@gmail.com>
2024-11-08 01:39:10 +0600

    main updated

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,20 +1,45 @@
-import 'package:flutter/material.dart';
-import 'login_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'login_page.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Uber-like App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: LoginPage(onTap: onTap),
+//     );
+//   }
+// }
 
-void main() {
-  runApp(MyApp());
+
+
+// import 'package:firebase_core/firebase_core.dart';
+import 'auth_page.dart';
+// import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Uber-like App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginScreen(),
+      home: AuthPage(),
     );
   }
 }
