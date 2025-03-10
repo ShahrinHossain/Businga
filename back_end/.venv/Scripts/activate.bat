e70fd5cb1485be6e0fea384b8ee5cf33eff5d6ee
Sadman-Rahman135 <sadmansahil2002pro@gmail.com>
2025-03-11 03:08:41 +0600

    prevcode

--- back_end/.venv/Scripts/activate.bat
+++ back_end/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\projects\2025\march\Businga\back_end\.venv
+set VIRTUAL_ENV=D:\software_flutter\willitrub\Businga\back_end\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- back_end/bus_app/serializers.py
+++ back_end/bus_app/serializers.py
@@ -7,20 +7,6 @@ from rest_framework.views import APIView
 from bus_app.models import Stoppage, Profile, OngoingTrip, Bus, Route, BusCompany
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password']
-#         extra_kwargs = {'password': {'write_only': True}}
-#
-#     def create(self, validated_data):
-#         user = User(
-#             email=validated_data['email'],
-#             username=validated_data['username'],
-#         )
-#         user.set_password(validated_data['password'])
-#         user.save()
-#         return user
 
 class UserSerializer(serializers.ModelSerializer):
     role = serializers.CharField()  # Add role field
